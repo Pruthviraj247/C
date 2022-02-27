82fd3eecb42d5c669a18298708da6957a55e70dd
Pruthviraj <pbhrashkal985@gmail.com>
2022-02-27 15:18:59 +0530

    Create 10_Time_Reverse.c

---
+++ 1.4 Decision Making/10_Time_Reverse.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int Num;
+    Num = 10;
+    while (Num>=1)
+    {
+        printf("%d",Num);
+        printf("\n");
+        Num--;
+    }
+    
+}
