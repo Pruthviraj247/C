a6c9d3f173053eb4b1ed15bd83c36e398bfc7827
Pruthviraj <pbhrashkal985@gmail.com>
2022-02-27 15:20:58 +0530

    Create 10_Even_Num.c

---
+++ 1.4 Decision Making/10_Even_Num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int Num;
+    Num = 1;
+    while (Num <= 10)
+    {
+        if (Num % 2 == 0)
+            printf("%d Is Even Number", Num);
+            printf("\n");
+        Num++;
+       
+    }
+}
