da00a3a8437b53109c7f078a7453498236335668
Pruthviraj <pbhrashkal985@gmail.com>
2022-02-27 15:19:29 +0530

    Create 10_Time_Hello.c

---
+++ 1.4 Decision Making/10_Time_Hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int Num;
+    Num = 1;
+    while (Num<=10)
+    {
+        printf("Hello");
+        printf("\n");
+        Num++;
+    }
+    
+}
